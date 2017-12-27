284aaca882e2d7921ec753ae22b77fc1df410cd7
allthatjava <allthatjava@yahoo.ca>
2017-12-27 11:43:36 -0500

    Initial push

---
+++ src/main/java/brian/boot/example/cloud/hystrix/BootExampleHystrixApplication.java
@@ -0,0 +1,12 @@
+package brian.boot.example.cloud.hystrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootExampleHystrixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootExampleHystrixApplication.class, args);
+	}
+}
